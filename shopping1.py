202258b0bc2ebb2d725dfc0e3edfaa3dbd065f62
Faeze <fkherad7596@gmail.com>
2018-10-29 13:28:17 +0330

    tamrin 1

---
+++ shopping1.py
@@ -0,0 +1,20 @@
+import time
+c = []
+d = []
+with open("C:\\Users\\Lenovo\\Desktop\\Misc\\instacart_online_grocery_shopping_2017_05_01\\instacart_2017_05_01\\order_products__prior.csv", "r")as order_products__prior:
+        for line in order_products__prior:
+            a = line.split(',')
+            b=a[1:2]
+            c.append(b[0])
+        for i in range(1,10000):
+            d.append(c.count(str(i)))
+        goal = d.index(min(d))
+with open("C:\\Users\\Lenovo\\Desktop\\Misc\\instacart_online_grocery_shopping_2017_05_01\\instacart_2017_05_01\\products.csv","r")as products:
+    w=time.time()
+    for line in products:
+        e=line.split(',')
+        if e[0] == str(goal):
+            print(e[1])
+    q=time.time()
+    x=q-w
+    print("time: "+str(x))
